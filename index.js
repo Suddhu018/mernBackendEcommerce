3496007fd17467bb54fc0c460441137e93a9797d
Suddhu018 <shekharsudhanshu018@gmail.com>
2024-01-21 11:26:28 +0530

    simple

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   //this is for multer
   destination: function (req, file, cb) {
-    return cb(null, "./public/");
+    return cb(null, "https://mern-front-end-ecommerce.vercel.app/public");
   },
   filename: function (req, file, cb) {
     req.image = `${Date.now()}${file.originalname}`;
